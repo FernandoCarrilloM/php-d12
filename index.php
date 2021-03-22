76cb56c7395d5ce317a779a96cb763295c9c81f4
Fernando Carrillo <fernando.carrillo5123@alumnos.udg.mx>
2021-03-22 10:20:07 -0600

    Modifica titulo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP intro</title>
+    <title>Introducción a PHP</title>
 </head>
 <body>
     <h1>Manejo de Tareas</h1>
