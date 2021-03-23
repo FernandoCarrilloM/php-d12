958c26fc9ab7506ef52a4208d978ba11691b137b
Fernando Carrillo <fernando.carrillo5123@alumnos.udg.mx>
2021-03-22 18:10:44 -0600

    Creación de concexion

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "php-d12";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    }
-?>
+<?php include('conexion.php'); ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- store.php
+++ store.php
@@ -1,17 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "php-d12";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    }
+    include('conexion.php'); 
+
     if(!empty($_POST['tarea'])){
         //Recibir datos
         $tarea = $_POST['tarea'];
